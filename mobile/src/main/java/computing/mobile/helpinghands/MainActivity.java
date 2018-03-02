6e0513a81ad7dbc1a418d281d52258cb6f63e18f
Rohit Prasad <rohitrp3@gmail.com>
2018-03-03 02:11:53 +0530

    Fix null pointer exception for start button

--- mobile/src/main/java/computing/mobile/helpinghands/MainActivity.java
+++ mobile/src/main/java/computing/mobile/helpinghands/MainActivity.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     public static Context thisAct;
-    private Button start;
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         thisAct = MainActivity.this;
 
-        start.setOnClickListener(new View.OnClickListener() {
+        mStartButton = findViewById(R.id.start);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(start.getText().toString().equalsIgnoreCase(getString(R.string.ride_start))){
-                    start.setText(getString(R.string.ride_stop));
+                if(mStartButton.getText().toString().equalsIgnoreCase(getString(R.string.ride_start))){
+                    mStartButton.setText(getString(R.string.ride_stop));
                     Intent serviceIntent = new Intent(MainActivity.this, GPSService.class);
                     startService(serviceIntent);
+                    Log.d(this.getClass().getName(), "Started");
                 }
                 else{
-                    start.setText(getString(R.string.ride_start));
+                    mStartButton.setText(getString(R.string.ride_start));
                     stopService(new Intent(MainActivity.this, GPSService.class));
                 }
             }
